2f77fb4267dde4e91b19a1d6e264fb7d905df989
Francisco José Ocampo Giraldo <96587479+fjocampog97@users.noreply.github.com>
2023-03-16 19:44:37 -0500

    incluyendo la estructura completa de archivos para el microcontrolador de la primera entrega

---
+++ lab3iot/button_deepsleep.py
@@ -0,0 +1,78 @@
+from os import statvfs
+from time import sleep
+import network
+from mqtt import MQTTClient 
+import machine 
+import time
+import json
+import random
+import bluetooth
+import dht
+from machine import Pin
+
+# Initialize boot button
+boot_btn = Pin(0, Pin.IN, Pin.PULL_UP)
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('NF702', '3006757878')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
+
+def sub_cb(topic, msg): 
+    jsonobject=json.loads(msg)
+    print(jsonobject["bulb"])
+    if jsonobject["bulb"]=="ON":
+       led.value(1)
+    elif jsonobject["bulb"]=="OFF":
+        led.value(0)
+
+led=machine.Pin(2,machine.Pin.OUT)
+
+with open("certs/cert", 'rb') as f:
+    certf = f.read()
+with open("certs/privkey", 'rb') as f:
+    keyf = f.read()
+
+client = MQTTClient(client_id="esp_32_class3_nrfj_thing", server="a1ztq6e5d2o4a0-ats.iot.us-east-1.amazonaws.com", port=8883, ssl=True, ssl_params={"cert":certf, "key":keyf})
+ 
+
+client.set_callback(sub_cb) 
+client.connect()
+client.subscribe(topic="esp32nrfj/sub")
+
+
+d = dht.DHT11(Pin(15))
+i=0
+
+
+i = (i + 1) % 10
+d.measure()
+    
+temp = d.temperature()
+hum = d.humidity()
+    
+print('Temperature: {}°C\tHumidity: {}%'.format(temp, hum))
+print("Sending temp/humidity data") 
+client.publish(topic="esp32nrfj/dht11data", msg=json.dumps({"temp": int(temp),"hum":int(hum)}))   
+    #machine.lightsleep(10000)
+time.sleep(10)
+
+# Check for boot button press
+if not boot_btn.value():
+    print("Boot button pressed, exiting deep sleep")
+else:
+    client.check_msg()
+    # Configurar pines para que estén en el estado correcto
+    led.value(0)
+    # Agregar una pausa antes de entrar en modo de suspensión profunda
+    #time.sleep(5)
+    # Entrar en modo de suspensión profunda
+    machine.deepsleep(10000)
